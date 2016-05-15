ef7164e8ebe0c37780c7f7b71ba9e595b2099978
Brendan Horan <brendan@horan.hk>
2016-05-15 13:46:00 +0800

    Update relay_toggle.py
    added comments

--- python/relay_toggle.py
+++ python/relay_toggle.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
+# brendan@horan.hk
+# relay_toggle.py
 
+# Needed imports
 import sys
 import os
 import serial
 import time
 
+# take the first cmd line arg
 device_selection = sys.argv[1]
 
-
+# Set up serial. This needs some error handling 
 relay_device = serial.Serial('/dev/ttyUSB0',
                             baudrate=9600,
                             bytesize=serial.EIGHTBITS,
@@ -21,7 +25,7 @@ relay_device = serial.Serial('/dev/ttyUSB0',
 # sleep a little to wait for the arduino to reset
 time.sleep(5)
 
-
+# issue commands to the arduino
 if device_selection == '1':
   relay_data_encode = str.encode("relay_1")
   relay_device.write(relay_data_encode)
